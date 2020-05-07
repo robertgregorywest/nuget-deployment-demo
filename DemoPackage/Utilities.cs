f8db3348aed4eb6047558c311d9588dfd81cdb6c
Rob West <robertgregorywest@gmail.com>
2020-05-07 13:04:21 +0100

    Added demo project

---
+++ DemoPackage/Utilities.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace DemoPackage
+{
+    public static class Utilities
+    {
+        public static string GetAssemblyVersion()
+        {
+            return Assembly.GetEntryAssembly()?.GetName().Version?.ToString();
+        }
+    }
+}
